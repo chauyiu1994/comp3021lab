42b5eda9f21e4cb72bbb2a018cdf1ce638ba4d10
chauchau <chauchau@chauchau-PC>
2016-10-04 21:01:48 +0800

    lab5

--- src/base/NoteBook.java
+++ src/base/NoteBook.java
@@ -2,11 +2,29 @@ package base;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.io.FileInputStream;	
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-public class NoteBook {
+public class NoteBook implements java.io.Serializable {
 	private ArrayList<Folder> folders;
+	private static final long serialVersionUID=1L;
 	public NoteBook(){
 		folders=new ArrayList<Folder>();
+	}
+	public NoteBook(String file){
+		FileInputStream fis=null;
+		ObjectInputStream in=null;
+		try{
+			fis=new FileInputStream("file.ser");
+			in=new ObjectInputStream(fis);
+			NoteBook n=(NoteBook) in.readObject();
+			folders=n.getFolders();
+			in.close();
+		}catch(Exception e){
+			folders=new ArrayList<Folder>();
+		}
 	}
 	public boolean createTextNote(String folderName,String title){
 		TextNote note=new TextNote(title);
@@ -56,6 +74,19 @@ public class NoteBook {
 			n.addAll(f.searchNotes(keywords));
 		}
 		return n;
+	}
+	public boolean save(String file){
+		FileOutputStream fos=null;
+		ObjectOutputStream out=null;
+		try{
+			fos=new FileOutputStream("file.ser");
+			out=new ObjectOutputStream(fos);
+			out.writeObject(this);
+			out.close();
+		}catch(Exception e){
+			return false;
+		}
+		return true;
 	}
     
 }
--- src/base/TextNote.java
+++ src/base/TextNote.java
@@ -1,13 +1,59 @@
 package base;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
-public class TextNote extends Note{
+public class TextNote extends Note {
 	private String content;
+	private static final long serialVersionUID=1L;
 	public TextNote(String title){
 		super(title);
 	}
 	public TextNote(String title,String content){
 		super(title);
 		this.content=content;
+	}
+	public TextNote(File f){
+		super(f.getName());
+		this.content=getTextFromFile(f.getAbsolutePath());
+		
+	}
+	private String getTextFromFile(String absolutePath){
+		String result="";
+		try(BufferedReader br = new BufferedReader(new FileReader(absolutePath))){
+			String sCurrentLine;
+			while((sCurrentLine=br.readLine())!=null){
+				result+=sCurrentLine;
+			}
+		}catch(IOException e){
+			return null;
+		}	
+		return result;
+	}
+	public void exportTextToFile(String pathFolder){
+		try{
+			File file=null;
+			if(pathFolder==""){
+				file=new File(pathFolder+this.getTitle().replaceAll(" ", "_")+".txt");
+			}else{
+				file=new File(pathFolder+File.separator+this.getTitle().replaceAll(" ", "_")+".txt");
+			}
+		    if(!file.exists()){
+		    	file.createNewFile();
+		    }
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+		}catch(IOException e){
+			return;
+		}
 	}
 	@Override
 	public boolean contains(String s){
