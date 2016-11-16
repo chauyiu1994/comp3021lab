422e3f6884e52a1ef1fbae2d9c366d43a24a4f28
chauchau <chauchau@chauchau-PC>
2016-11-16 20:16:59 +0800

    LAB10

---
+++ src/lab10/FindMax.java
@@ -0,0 +1,87 @@
+package lab10;
+import java.util.concurrent.*;
+
+/**
+ * 
+ * COMP 3021
+ * 
+This is a class that prints the maximum value of a given array of 90 elements
+
+This is a single threaded version.
+
+Create a multi-thread version with 3 threads:
+
+one thread finds the max among the cells [0,29] 
+another thread the max among the cells [30,59] 
+another thread the max among the cells [60,89]
+
+Compare the results of the three threads and print at console the max value.
+
+ * 
+ * @author valerio
+ *
+ */
+public class FindMax {
+	// this is an array of 90 elements
+	// the max value of this array is 9999
+	static int[] array = { 1, 34, 5, 6, 343, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2, 3, 4543,
+			234, 3, 454, 1, 2, 3, 1, 9999, 34, 5, 6, 343, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2,
+			3, 4543, 234, 3, 454, 1, 2, 3, 1, 34, 5, 6, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2,
+			3, 4543, 234, 3, 454, 1, 2, 3 };
+
+	public static void main(String[] args) {
+		new FindMax().printMax();
+	    new FindMax().printMaxByThread();
+		
+	}
+	
+	public void printMaxByThread(){
+		int[] max1=new int[3];
+		ExecutorService executor= Executors.newCachedThreadPool();
+		executor.execute(new Runnable(){
+			@Override
+			public void run(){
+				max1[0]=findMax(0,29);
+			}
+		});
+		executor.execute(new Runnable(){
+			@Override
+			public void run(){
+				max1[1]=findMax(30,59);
+			}
+		});
+		executor.execute(new Runnable(){
+			@Override
+			public void run(){
+				max1[2]=findMax(60,89);
+			}
+		});
+		System.out.println("By multi-thread: "+Math.max(max1[2],Math.max(max1[0],max1[1])));
+		
+		
+	}
+
+	public void printMax() {
+		// this is a single threaded version
+		int max = findMax(0, array.length - 1);
+		System.out.println("the max value is " + max);
+	}
+
+	/**
+	 * returns the max value in the array within a give range [begin,range]
+	 * 
+	 * @param begin
+	 * @param end
+	 * @return
+	 */
+	private int findMax(int begin, int end) {
+		// you should NOT change this function
+		int max = array[begin];
+		for (int i = begin + 1; i <= end; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		return max;
+	}
+}
